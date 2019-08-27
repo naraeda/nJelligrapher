4b9aad2964fccb1f549079f8a87264482271cc1b
Ellie Z <ellie@Ellies-MacBook-Air.local>
2019-08-27 16:34:33 -0700

    add event dashboard

--- src/main/java/com/codingdojo/groupproject/controllers/EventCtrl.java
+++ src/main/java/com/codingdojo/groupproject/controllers/EventCtrl.java
@@ -1,5 +1,61 @@
 package com.codingdojo.groupproject.controllers;
 
-public class EventCtrl {
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.codingdojo.groupproject.models.Event;
+import com.codingdojo.groupproject.models.User;
+import com.codingdojo.groupproject.models.UserEvent;
+import com.codingdojo.groupproject.services.EventServ;
+import com.codingdojo.groupproject.services.UserServ;
+
+@Controller 
+public class EventCtrl {
+	
+	@Autowired 
+	private EventServ eS;
+	
+	@Autowired
+	private UserServ uS; 
+	
+	 @GetMapping("/events")
+	    public String home(@ModelAttribute("event") Event event, HttpSession session, Model model, RedirectAttributes rA) {
+	    	if(session.getAttribute("userId")==null) {
+	    		rA.addFlashAttribute("NonLoggedInError", "Please log in first!");
+	    		return "redirect:/";
+	    	}
+	    	else {
+	    		
+		    		Long userId = (Long) session.getAttribute("userId");
+		    		User loggedInUser = uS.findUserById(userId);
+		    		model.addAttribute("user", loggedInUser);
+		    		model.addAttribute("eventsInState", eS.getEventByState(loggedInUser.getState()));
+		    		model.addAttribute("eventsOutState", eS.getEventByOppState(loggedInUser.getState()));
+		    		model.addAttribute("middleTable", new UserEvent());
+		    		return "eventDashboard.jsp";
+	    		}
+	    }
+	
+	@PostMapping("/events")
+	public String addEvents(@Valid @ModelAttribute("event") Event event, BindingResult result, RedirectAttributes rA) {
+		
+		if(result.hasErrors()) {
+			rA.addFlashAttribute("invalidEventInfo", "Fields cannot be empty!" );
+			return "redirect:/events";
+		}
+		else {
+			eS.createEvent(event);
+			return "redirect:/events";
+		}
+	}
+	
 }
--- src/main/java/com/codingdojo/groupproject/services/EventServ.java
+++ src/main/java/com/codingdojo/groupproject/services/EventServ.java
@@ -1,5 +1,45 @@
 package com.codingdojo.groupproject.services;
 
-public class EventServ {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.groupproject.models.Event;
+import com.codingdojo.groupproject.repositories.EventRepo;
+
+@Service
+public class EventServ {
+	
+	@Autowired
+	private EventRepo eR;
+	
+	public List<Event> getAllEvents(){
+		return eR.findAll();
+	}
+	
+	public Event getEventById(Long id) {
+		Optional<Event> optEvent = eR.findById(id);
+		if(optEvent.isPresent()) {
+			return optEvent.get();
+		}
+		return null;
+	}
+	
+	public Event createEvent(Event event) {
+		return eR.save(event);
+	}
+	
+	public List<Event> getEventByState(String state) {
+		return eR.findAllByStateOrderByDateDesc(state);
+	}
+	public List<Event> getEventByOppState(String state){
+		return eR.findAllByStateNotContaining(state);
+	}
+
+	public void deleteEvent(Long id) {
+		eR.deleteById(id);
+	}
+	
 }
